04bcae0efa5b12d5c5513d76e470595992bdbb5e
Eben-Success <aebenezer237@gmail.com>
2022-07-10 23:50:33 +0000

    added binary search file

--- binary_search.py
+++ binary_search.py
@@ -3,4 +3,11 @@ def binary_search(list, item):
     high = len(list) -1
 
     while low <= high:
-        mid = (low + high)
+        mid = (low + high) / 2
+        guess = list[mid]
+
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid -1
+
