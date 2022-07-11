9a7236636e2599461ca21cd127e67caeb587e4f8
Eben-Success <aebenezer237@gmail.com>
2022-07-11 00:45:38 +0000

    adding comments to code

--- binary_search.py
+++ binary_search.py
@@ -1,15 +1,19 @@
+# Python 3 program for recursive binary search
+
 # Return index of x in arr if present, else -1
-
 def binary_search(arr, low, high, x):
-    # Check base case
-    if high >= low:
-        mid = (high + low) // 2
 
-        # If element is present at the middle itself
+    #
+    if high >= low:
+        mid = (low + high) // 2
+
         if arr[mid] == x:
             return mid
 
-        # If element is smaller than mid, then it can only
-        # be present in the left subarry
         elif arr[mid] > x:
             return binary_search(arr, low, mid - 1, x)
+        else:
+            return binary_search(arr, mid + 1, high, x)
+
+    else:
+        return -1
