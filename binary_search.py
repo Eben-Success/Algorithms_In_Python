046b53790007dd33dfc790c7c427529939e5dab0
Eben-Success <aebenezer237@gmail.com>
2022-07-11 00:52:02 +0000

    comment done

--- binary_search.py
+++ binary_search.py
@@ -25,13 +25,13 @@ def binary_search(arr, low, high, x):
         return -1
 
     # Test array
-    arr = [ 2, 3, 4, 10, 40 ]
-    x = 10
+arr = [ 2, 3, 4, 10, 40 ]
+x = 4
 
-    result = binary_search(arr, 0, len(arr) - 1, x)
+result = binary_search(arr, 0, len(arr) - 1, x)
 
-    if result != -1:
-        presnet("Element is present at index", str(result))
-    else:
-        print("Element is not present in array")
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
 
