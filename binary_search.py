965648edb18198780577304be0007322e0a54a32
Eben-Success <aebenezer237@gmail.com>
2022-07-11 00:31:41 +0000

    algo continue

--- binary_search.py
+++ binary_search.py
@@ -1,3 +1,12 @@
-# Binary Search for GeeksforGeeks
-
 # Return index of x in arr if present, else -1
+
+def binary_search(arr, low, high, x):
+    # Check base case
+    if high >= low:
+        mid = (high + low) // 2
+
+        # If element is present at the middle itself
+        if arr[mid] == x:
+            return mid
+
+        # If element is smaller
