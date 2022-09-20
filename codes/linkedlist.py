f10e890b3929db41c273b2e04029352a96b9ee29
Eben-Success <aebenezer237@gmail.com>
2022-09-20 12:33:04 +0000

    update

---
+++ codes/linkedlist.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_begining(self, data):
+        node = Node(data, self.head)
+
+
+if __name__ == '__main__':
+    pass
