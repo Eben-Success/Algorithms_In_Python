41ff6aedf72c291f779f853e9cf7bec9d1295b23
eben-success <aebenezer237@gmail.com>
2022-09-30 11:18:33 +0000

    update

---
+++ codes/Selection_Sort.py
@@ -0,0 +1,10 @@
+"""
+Insertion Sort Algorithm
+
+1. Iterate from arr[i] to arr[N] over the array
+2. Compare the current element (key) to its predecessor
+3. If the key element is smaller than its predecessor, compare it to the elements before.
+Move the greater elements one position up to make space for the swapped element.
+"""
+
+# Code Implementation
