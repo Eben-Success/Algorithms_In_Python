1c691e2291f7f0146f4034e5d92054514e3729c7
Eben-Success <aebenezer237@gmail.com>
2022-10-09 21:19:41 +0000

    linkedlist implementation

--- codebasics/LinkedList.py
+++ codebasics/LinkedList.py
@@ -1,2 +1,19 @@
 class Node:
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+        def print(self):
+            if self.head is None:
+                print("Linked list is empty")
+                return
+            itr = self.head
+            llstr = ''
+            while itr:
+                llstr += str(data) + '-->' if itr.next else str(itr.data)
+                itr = itr.next
+            print(llstr)
