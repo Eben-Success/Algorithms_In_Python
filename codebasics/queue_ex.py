3db1fe61bdf84fd25c8a68f32de785da49fb75cd
Eben-Success <aebenezer237@gmail.com>
2022-10-10 19:46:19 +0000

    Queue implementation

--- codebasics/queue_ex.py
+++ codebasics/queue_ex.py
@@ -3,3 +3,17 @@ from collections import deque
 class Queue:
     def __init__(self):
         self.buffer = deque()
+
+    def enqueue(self, val):
+        self.buffer.append(val)
+
+    def dequeue(self):
+        return self.buffer.pop()
+
+    def is_empty(self):
+        return len(self.buffer) == 0
+
+    def seize(self):
+        return len(self.buffer)
+
+    def place_order
