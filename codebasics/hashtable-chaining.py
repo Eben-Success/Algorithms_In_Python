9da6d1713f83e0b7062f3c7e5670f601dbff844a
Eben-Success <aebenezer237@gmail.com>
2022-10-10 20:07:15 +0000

    Implementing HashTable

--- codebasics/hashtable-chaining.py
+++ codebasics/hashtable-chaining.py
@@ -2,3 +2,10 @@ class HashTable:
     def __init__(self):
         self.Max = 10
         self.arr = [None for i in range(self.Max)]
+
+    def get_hash(self, key):
+        hash = 0
+        for char in key:
+            hash += ord(char)
+        return hash % self.Max
+
