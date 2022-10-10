b643b1ada50d4333838285a21182a832cd3f8207
Eben-Success <aebenezer237@gmail.com>
2022-10-10 20:04:44 +0000

    Queue implementation

---
+++ codebasics/hashtable.py
@@ -0,0 +1,18 @@
+class HashTable:
+    def __init__(self):
+        self.Max = 100
+        self.arr = [None for i in range(self.Max)]
+
+    def get_hash(self, key):
+        hash = 0
+        for char in key:
+            hash += ord(char)
+            return hash % self.Max
+
+    def __getitem__(self, index):
+        h = self.get_hash(index)
+        return self.arr[h]
+
+    def __setitem(self, key, val):
+        h = self.get_hash(key)
+        self.arr[h] = val
