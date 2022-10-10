cc197150c1e8c789979f3aaa1a65bf9da8c8fa66
Eben-Success <aebenezer237@gmail.com>
2022-10-10 12:21:58 +0000

    linkedlist implementation

--- codebasics/LinkedList.py
+++ codebasics/LinkedList.py
@@ -1,7 +1,8 @@
 class Node:
-    def __init__(self, data=None, next=None):
+    def __init__(self, data=None, next=None, prev=None):
         self.data = data
         self.next = next
+        self.prev = prev
 
 class LinkedList:
     def __init(self):
@@ -111,6 +112,7 @@ class LinkedList:
                 curr.next = curr.next.next
                 break
             curr = curr.next
+
 
 
 
