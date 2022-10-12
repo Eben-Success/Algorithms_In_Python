12919d9a5241065f9dac7de58d1b7989ae7d0d9e
Eben-Success <aebenezer237@gmail.com>
2022-10-12 20:52:46 +0000

    Implementing Tree

---
+++ codebasics/Trees.py
@@ -0,0 +1,31 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        self.parent = None
+
+    def get_level(self):
+        level = 0
+        p = self.parent
+        while p:
+            level += 1
+            p = p.parent
+        return level
+
+    def print_tree(self):
+        spaces = ' ' * self.get_level() * 3
+        prefix = spaces + "|__" if self.parent else " "
+        print(prefix + self.data)
+        if self.children:
+            for child in self.children:
+                child.print_tree()
+
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
+
+
+
+def build_product_tree():
+
+
