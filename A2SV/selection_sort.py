4557835c67d2538959a9a6ac234ef654aa55bd86
Eben-Success <aebenezer237@gmail.com>
2022-11-04 08:17:19 +0000

    implementing binary tree

--- A2SV/selection_sort.py
+++ A2SV/selection_sort.py
@@ -9,3 +9,11 @@ def selectionSort(array, size):
 
         for i in range(step+1, size):
 
+            # to sort in descending order, change > to < in this line
+            # select the minimum element in each loop
+
+            if array[i] < array[min_idx]:
+                min_idx = i
+
+            (array[step], array[min_idx]) = (array[min_idx], array[step])
+
