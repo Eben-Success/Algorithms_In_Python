67678f3e8d27584197c4c87ae7cd712d657c6311
Eben-Success <aebenezer237@gmail.com>
2022-11-04 16:15:07 +0000

    Implementation of bubble sort

--- A2SV/Bubble Sort/Bubble Sort.py
+++ A2SV/Bubble Sort/Bubble Sort.py
@@ -6,10 +6,18 @@ are in the intended order
 # Bubble sort in Python
 
 def BubbleSort(array):
+
+    # loop to access each array element
     for i in range(len(array)):
+
+        # loop to comapare array elements
+        # change > to < to sort in descending order
         for j in range(0, len(array) - i - 1):
             if array[j] > array[j + 1]:
                 temp = array[j]
+
+                # swapping elements if elements
+                # are not in intended order.
                 array[j] = array[j + 1]
                 array[j + 1] = temp
 
