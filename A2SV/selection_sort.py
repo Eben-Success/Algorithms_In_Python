6585b86229fa3221c4439330a7ad153c34a3c55e
Eben-Success <aebenezer237@gmail.com>
2022-11-04 08:19:35 +0000

    implementing binary tree

--- A2SV/selection_sort.py
+++ A2SV/selection_sort.py
@@ -2,12 +2,12 @@
 in each iteration and places that element at the beginining of the unsorted list.
 """
 
-def selectionSort(array, size):
 
+def selectionSort(array, size):
     for step in range(size):
         min_idx = step
 
-        for i in range(step+1, size):
+        for i in range(step + 1, size):
 
             # to sort in descending order, change > to < in this line
             # select the minimum element in each loop
@@ -15,5 +15,11 @@ def selectionSort(array, size):
             if array[i] < array[min_idx]:
                 min_idx = i
 
-            (array[step], array[min_idx]) = (array[min_idx], array[step])
+        (array[step], array[min_idx]) = (array[min_idx], array[step])
 
+
+data = [-2, 45, 0, 11, -9]
+size = len(data)
+selectionSort(data, size)
+print("Sorted Array in Asceding Order: ")
+print(data)
