588b3cf37bb5095770377d9a75f1c501ae8e31ec
Eben-Success <aebenezer237@gmail.com>
2022-11-06 08:33:41 +0000

    Implementation of merge sort

--- A2SV/Merge Sort/MergeSort.py
+++ A2SV/Merge Sort/MergeSort.py
@@ -1,15 +1,20 @@
 def mergeSort(array):
     if len(array) > 1:
 
+        # m is the point where the array is divided into two subarrays.
         m = len(array) // 2
 
         L = array[:m]
         R = array[m:]
 
+        # Sort the two halves
         mergeSort(L)
         mergeSort(R)
 
         i = j = k = 0
+
+        # Until we reach either end of L or M, pick the larger among
+        # elements L and M and place them in the correct position at A[p..r]
 
         while i < len(L) and j < len(R):
             if L[i] < R[j]:
@@ -29,3 +34,21 @@ def mergeSort(array):
             array[k] = R[j]
             j+=1
             k+=1
+
+
+
+# Print the Array
+def printList(array):
+    for i in range(len(array)):
+        print(array[i], end=' ')
+    print()
+
+
+# Driver program
+if __name__ == '__main__':
+    array = [45, -13, 2, 232, 1, 3, 6, 7, 8]
+
+    mergeSort(array)
+
+    print("Sorted array is: ")
+    printList(array)
