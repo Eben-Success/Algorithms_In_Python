a44bf94d75f4fcf005c5f246e510f9987ba11d77
Eben-Success <aebenezer237@gmail.com>
2022-11-06 10:58:16 +0000

    Implementation of merge sort

--- A2SV/Merge Sort/MergeSort.py
+++ A2SV/Merge Sort/MergeSort.py
@@ -19,22 +19,21 @@ def mergeSort(array):
         while i < len(L) and j < len(R):
             if L[i] < R[j]:
                 array[k] = L[i]
-                i+=1
+                i += 1
             else:
                 array[k] = R[j]
-                j+=1
-            k+=1
+                j += 1
+            k += 1
 
         while i < len(L):
             array[k] = L[i]
-            i+=1
-            k+=1
+            i += 1
+            k += 1
 
         while j < len(R):
             array[k] = R[j]
-            j+=1
-            k+=1
-
+            j += 1
+            k += 1
 
 
 # Print the Array
