3e84b082463fb04748f1dcc75f685e740410e987
Eben-Success <aebenezer237@gmail.com>
2022-11-06 11:07:36 +0000

    Implementation of merge sort

--- A2SV/Bubble Sort/Counting Sort/coutingSort.py
+++ A2SV/Bubble Sort/Counting Sort/coutingSort.py
@@ -1,3 +1,6 @@
+# Counting sort in Python programming
+
+
 def countingSort(array):
     size = len(array)
     output = [0] * size
@@ -9,13 +12,13 @@ def countingSort(array):
     for i in range(0, size):
         count[array[i]] += 1
 
-    # store the cumulative count
+    # Store the cummulative count
     for i in range(1, 10):
-        count[i] += count[i-1]
+        count[i] += count[i - 1]
 
     # Find the index of each element of the original array in count array
     # place the elements in output array
-
+    i = size - 1
     while i >= 0:
         output[count[array[i]] - 1] = array[i]
         count[array[i]] -= 1
@@ -25,7 +28,8 @@ def countingSort(array):
     for i in range(0, size):
         array[i] = output[i]
 
-data = [4, 5, 2, 6, 2, 6, 7, 3, 23 ,-23, 23]
+
+data = [4, 2, 2, 8, 3, 3, 1]
 countingSort(data)
-print("Sorted Array in Ascending order: ")
+print("Sorted Array in Ascending Order: ")
 print(data)
