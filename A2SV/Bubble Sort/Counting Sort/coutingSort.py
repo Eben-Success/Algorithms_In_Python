8cb48a683d490f315cee9fc952105e47447eb4a8
Eben-Success <aebenezer237@gmail.com>
2022-11-06 11:01:02 +0000

    Implementation of merge sort

--- A2SV/Bubble Sort/Counting Sort/coutingSort.py
+++ A2SV/Bubble Sort/Counting Sort/coutingSort.py
@@ -6,3 +6,14 @@ def countingSort(array):
     count = [0] * 10
 
     # Store the count of each elements in count array
+    for i in range(0, size):
+        count[array[i]] += 1
+
+    # store the cumulative count
+    for i in range(1, 10):
+        count[i] += count[i-1]
+
+    # Find the index of each element of the original array in count array
+    # place the elements in output array
+
+
